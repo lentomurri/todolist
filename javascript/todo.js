d78cdb683cef0815bf549c35b7c1ee98b116b0b9
[Lentomurri] <[dibonaventurasilvia@gmail.com]>
2018-12-20 20:16:46 +0000

    Green and pink colour

--- javascript/todo.js
+++ javascript/todo.js
@@ -1,6 +1,43 @@
-$("i").mouseenter( function() {
-  $(this).parent().css("backgroundColor", "yellow"));
+let ul = $("ul");
+let li = $("li");
+
+//cross over to do
+ul.on("click", "li", function() {
+  $(this).toggleClass("linethrough");
 })
-$("i").mouseleave( function() {
-  $(this).parent().css("backgroundColor", "transparent");
-})
+
+//click on bin icon to remove
+ul.on("click", "i", function(ev) {
+    //fadeOut: the milliseconds for the fade out +
+    //the remove function happens at THE END of the Fadeout
+      $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+      });
+      //stops events piling up from child to parents
+      ev.stopPropagation();
+      ev.preventDefault();
+});
+
+//create new to do
+let input = $("input[type='text']");
+
+
+input.keydown( function(e) {
+  //set enter as the key to record the to do
+  var key = e.which;
+  if(key === 13) {
+    let todo = input.val();
+    $(this).val("");
+    todo = "<li><i class='fas fa-eraser'></i>" + todo + "</li>";
+    ul.append(todo);
+  }
+}
+);
+
+// //hover over icon to highlight which toDo is selected
+// ul.on("mouseenter", "li", function() {
+//   $(this).addClass("listhover")
+// });
+// ul.on("mouseleave", "li", function() {
+//   $(this).removeClass("listhover")
+// });

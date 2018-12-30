6f8f7a303fad5165433d878750c3b01e6af21e8a
[Lentomurri] <[dibonaventurasilvia@gmail.com]>
2018-12-30 17:10:53 +0000

    Fixes icons, adds toggle buttons, code clean

--- javascript/todo.js
+++ javascript/todo.js
@@ -8,33 +8,33 @@ ul.on("click", "li", function() {
 
 //click on bin icon to remove
 ul.on("click", "span", function(ev) {
-    //fadeOut: the milliseconds for the fade out +
-    //the remove function happens at THE END of the Fadeout
-      $(this).parent().fadeOut(500, function(){
-        $(this).remove();
-      });
-      //stops events piling up from child to parents
-      ev.stopPropagation();
-      ev.preventDefault();
+  //fadeOut: the milliseconds for the fade out +
+  //the remove function happens at THE END of the Fadeout
+  $(this).parent().fadeOut(500, function() {
+    $(this).remove();
+  });
+  //stops events piling up from child to parents
+  ev.stopPropagation();
+  ev.preventDefault();
 });
 
 //create new to do
 let input = $("input[type='text']");
-let todoList = [];
-localStorage.setItem("todo", todoList)
 
-
-input.keydown( function(e) {
+input.keydown(function(e) {
   //set enter as the key to record the to do
   var key = e.which;
-  if(key === 13) {
-    let todo = input.val();
+  if (key === 13) {
+    todo = input.val();
     $(this).val("");
-    todo = "<li><span><i class='far fa-trash-alt'></i></span>" + todo + "</li>";
-    ul.append(todo);
+    let list = "<li><span><i class='far fa-trash-alt'></i></span>" + todo + "</li>";
+    ul.append(list);
   }
-}
-);
+});
+
+$("#toggle").on("click", function() {
+  input.fadeToggle();
+})
 
 
 
